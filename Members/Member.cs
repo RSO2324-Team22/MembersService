f3f00b76ab7432d119fd8ef8d0f94675d416ac48
Matrkorac <matic.koracin@gmail.com>
2023-12-06 16:40:45 +0100

    property is not mapped

--- Members/Member.cs
+++ Members/Member.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MembersService.Members;
 
 public class Member {
@@ -6,5 +8,6 @@ public class Member {
     public required Section Section { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
+    [NotMapped]
     public IEnumerable<Role> Roles { get; init; } = new List<Role>();
 }
